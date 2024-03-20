1b020acf8c726901baba5066aac687142512f459
Nicolas Perveaux <perveauxnicolas@gmail.com>
2024-03-20 16:36:36 +0100

    20 mars 2024
    Creation interface et timerVisuel

---
+++ Minutine/Controller/MorningRoutineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MorningRoutineViewController.swift
+//  Minutine
+//
+//  Created by Perveaux Nicolas on 20/03/2024.
+//
+
+import UIKit
+
+class MorningRoutineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var washButton: UIButton!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
